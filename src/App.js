eb177be2d31c577fa854492c67d4371104a13d05
Gabriel Poma <lgabrielpoma@gmail.com>
2023-09-12 08:54:39 +0200

    FRONT AGREGATEEEE

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar } from './components/navegacion/Navbar';
-import Login from './components/Auth/Login'; // Importa el componente Login
-import Signup from './components/Auth/SignUp'; // Importa el componente Signup
+import Login from './components/Auth/Login';
+import Signup from './components/Auth/SignUp';
 import Inicio from './components/Paginas/Inicio';
 
 function App() {
@@ -13,8 +13,8 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Inicio/>} />
-          <Route path="/login" element={<Login />} /> {/* Ruta para el login */}
-          <Route path="/signup" element={<Signup />} /> {/* Ruta para el registro */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </div>
     </Router>
